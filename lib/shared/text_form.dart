629de49eabe10d93f5a340806447de2e30f11ddf
Bewizi <toluwalase.bewaji@yahoo.com>
2025-06-25 07:28:01 +0100

    feat: Add TextForm widget
    This commit introduces a new `TextForm` widget, which is a reusable `TextFormField` with custom styling and functionality.
    
    - **Properties**:
        - `text`: Label and hint text for the field.
        - `validator`: Optional validation function.
        - `isPassword`: Boolean to indicate if it's a password field (defaults to `false`).
        - `keyboardType`: The type of keyboard to display (defaults to `TextInputType.text`).
        - `controller`: Optional `TextEditingController`.
        - `onSaved`: Optional callback function when the form is saved.
    - **Features**:
        - If `isPassword` is true, it displays a toggle icon to show/hide the password.
        - Custom styling for text, hint, label, and border.
        - The border is an `OutlineInputBorder` with a radius of 15.

---
+++ lib/shared/text_form.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+
+class TextForm extends StatefulWidget {
+  const TextForm(
+    this.text, {
+    super.key,
+    this.validator,
+    this.isPassword = false,
+    this.keyboardType = TextInputType.text,
+    this.controller,
+    this.onSaved,
+  });
+
+  final String text;
+  final String? Function(String?)? validator;
+  final bool isPassword;
+  final TextInputType keyboardType;
+  final TextEditingController? controller;
+  final void Function(String?)? onSaved;
+
+  @override
+  State<TextForm> createState() => _TextFormState();
+}
+
+class _TextFormState extends State<TextForm> {
+  bool _obscureText = true;
+
+  void _togglePasswordVisibility() {
+    setState(() {
+      _obscureText = !_obscureText;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      obscureText: widget.isPassword ? _obscureText : false,
+      validator: widget.validator,
+      keyboardType: widget.keyboardType,
+      controller: widget.controller,
+      style: TextStyle(fontSize: 20, color: Colors.black),
+      decoration: InputDecoration(
+        fillColor: Colors.red,
+        focusColor: Colors.green,
+        hintStyle: TextStyle(fontSize: 20, color: Colors.black),
+        labelStyle: TextStyle(fontSize: 20, color: Colors.black),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(15),
+          borderSide: BorderSide(color: Colors.black),
+        ),
+        hintText: 'Enter your  ${widget.text}',
+        label: Text(widget.text),
+        suffixIcon: widget.isPassword
+            ? IconButton(
+                onPressed: _togglePasswordVisibility,
+                icon: Icon(
+                  _obscureText ? Icons.visibility_off : Icons.visibility,
+                  color: Colors.black,
+                ),
+              )
+            : null,
+      ),
+      onSaved: widget.onSaved,
+    );
+  }
+}
