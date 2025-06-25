96d8573ba1f97331d02d6e095b62ade853cd8a83
Bewizi <toluwalase.bewaji@yahoo.com>
2025-06-25 07:27:34 +0100

    feat: Implement Register screen and functionality
    This commit introduces a new `Register` screen and the associated `registerUser` function for user registration.
    
    **Register Screen (`Register`)**:
    - A `StatefulWidget` that provides a user interface for creating a new account.
    - It features a header with a background image (`assets/images/recycle_symbol.png`) and a welcome message.
    - Input fields for full name, email, phone number, and password are provided using the `TextForm` widget.
    - Validation is implemented for all fields:
        - Name: Cannot be empty.
        - Email: Cannot be empty and must contain "@".
        - Phone Number: Cannot be empty.
        - Password: Cannot be empty, must be at least 6 characters long, and not more than 20 characters.
    - A `SubmitButton` is used for initiating the registration process. It shows a loading indicator when `isRegistering` is true.
    - On successful registration:
        - The form is reset, and input fields are cleared.
        - A modal (`ShowModal.showModal`) is displayed (commented out navigation to `Home` screen).
    - On failed registration:
        - A `SnackBar` displays an error message.
    - "Terms of Service" and "Privacy Policy" links are displayed.
    - A "Already have an account? Login" text allows navigation to the `Login` screen.
    
    **Register User Function (`registerUser`)**:
    - An asynchronous function that sends a POST request to the registration API endpoint (`http://192.168.1.162/ecobin/api/register.php`).
    - It takes `name`, `email`, `phone`, and `password` as required parameters.
    - The request body is JSON encoded.
    - It returns a `Map<String, dynamic>` indicating the success status, a message, and user data upon successful registration.

---
+++ lib/screens/register/register.dart
@@ -0,0 +1,295 @@
+import 'package:ecobin/screens/home/home.dart';
+import 'package:ecobin/screens/login/login.dart';
+import 'package:ecobin/screens/register/register_user.dart';
+import 'package:ecobin/shared/button.dart';
+import 'package:ecobin/shared/modal.dart';
+import 'package:ecobin/shared/text_form.dart';
+
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+
+class Register extends StatefulWidget {
+  const Register({super.key, this.isRegistering = true});
+
+  final bool isRegistering;
+
+  @override
+  State<Register> createState() => _RegisterState();
+}
+
+class _RegisterState extends State<Register> {
+  final _formKey = GlobalKey<FormState>();
+
+  final _nameController = TextEditingController();
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+  final _phoneController = TextEditingController();
+
+  bool isRegistering = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      //   background image
+      backgroundColor: Colors.white,
+      body: Column(
+        children: [
+          Stack(
+            children: [
+              Container(
+                width: double.infinity,
+                height: 230,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage('assets/images/recycle_symbol.png'),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+              Container(
+                width: double.infinity,
+                height: 230,
+                color: Color.fromRGBO(84, 180, 104, 0.8),
+              ),
+              Positioned(
+                top: 70,
+                left: 20,
+                right: 20,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Let’s Get You Started',
+                      style: TextStyle(
+                        fontSize: 32,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(height: 8),
+                    Text(
+                      'Create an account to keep your\n environment clean.',
+                      style: TextStyle(
+                        fontWeight: FontWeight.w600,
+                        fontSize: 16,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+
+          Expanded(
+            child: SingleChildScrollView(
+              padding: EdgeInsets.symmetric(vertical: 16, horizontal: 24),
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  children: [
+                    // input for users Name
+                    TextForm(
+                      'Full Name',
+                      controller: _nameController,
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter your name';
+                        }
+                        return null;
+                      },
+                    ),
+
+                    SizedBox(height: 20),
+
+                    // input for users Email
+                    TextForm(
+                      'Email',
+                      controller: _emailController,
+                      keyboardType: TextInputType.emailAddress,
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter your email';
+                        }
+                        if (!value.contains('@')) {
+                          return 'Please enter a valid email';
+                        }
+                        return null;
+                      },
+                    ),
+
+                    SizedBox(height: 20),
+
+                    // input for users Phone Number
+                    TextForm(
+                      'Phone Number',
+                      controller: _phoneController,
+                      keyboardType: TextInputType.phone,
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter your phone number';
+                        }
+                        return null;
+                      },
+                    ),
+                    SizedBox(height: 20),
+
+                    // input for users Password
+                    TextForm(
+                      'Password',
+                      controller: _passwordController,
+                      isPassword: true,
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter your password';
+                        }
+                        if (value.length < 6) {
+                          return 'Password must be at least 6 characters above';
+                        }
+                        if (value.length > 20) {
+                          return 'Password must be at most not be more 20 characters';
+                        }
+                        return null;
+                      },
+                    ),
+
+                    SizedBox(height: 20),
+
+                    // Submit form
+                    SubmitButton(
+                      'Create Account',
+                      isLoading: isRegistering,
+                      onPressed: () async {
+                        if (_formKey.currentState!.validate()) {
+                          setState(() {
+                            isRegistering = true;
+                          });
+
+                          _formKey.currentState!.save();
+
+                          try {
+                            final result = await registerUser(
+                              name: _nameController.text.trim(),
+                              email: _emailController.text.trim(),
+                              phone: _phoneController.text.trim(),
+                              password: _passwordController.text.trim(),
+                            );
+
+                            if (result['success'] == true) {
+                              // print('Registration Successful: ${result['user']}');
+
+                              _formKey.currentState!.reset();
+
+                              _nameController.clear();
+                              _emailController.clear();
+                              _passwordController.clear();
+                              _phoneController.clear();
+
+                              ShowModal.showModal(context);
+
+                              // await Future.delayed(Duration(seconds: 1000));
+                              //
+                              // Navigator.pushAndRemoveUntil(
+                              //   context,
+                              //   MaterialPageRoute(
+                              //     builder: (ctx) =>
+                              //         Home(result['user']['name']),
+                              //   ),
+                              //   (route) => false,
+                              // );
+                            } else {
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                SnackBar(
+                                  content: Text(result['message']),
+                                  padding: EdgeInsets.symmetric(
+                                    horizontal: 8.0,
+                                  ),
+                                  backgroundColor: Colors.green,
+                                  action: SnackBarAction(
+                                    label: 'Close',
+                                    textColor: Colors.white,
+                                    onPressed: () {},
+                                  ),
+                                ),
+                              );
+                              print('Registration failed:${result['message']}');
+                            }
+                          } catch (e) {
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              SnackBar(
+                                content: Text('An error occurred: $e'),
+                                backgroundColor: Colors.red,
+                              ),
+                            );
+                          } finally {
+                            setState(() {
+                              isRegistering = false;
+                            });
+                          }
+                        }
+                      },
+                    ),
+
+                    // Terms and conditions
+                    SizedBox(height: 20),
+                    Align(
+                      alignment: Alignment.center,
+                      child: RichText(
+                        text: TextSpan(
+                          text: 'By signing up, you agree to EcoBin’s',
+                          style: TextStyle(color: Colors.black, fontSize: 16),
+                          children: [
+                            TextSpan(
+                              text: ' Terms of \nService',
+                              style: TextStyle(color: Colors.green, height: 2),
+                            ),
+                            TextSpan(text: ' and '),
+                            TextSpan(
+                              text: 'Privacy Policy',
+                              style: TextStyle(color: Colors.green),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+
+                    SizedBox(height: 20),
+
+                    // text for already have an account
+                    Align(
+                      alignment: Alignment.center,
+                      child: RichText(
+                        text: TextSpan(
+                          text: 'Already have an account?',
+                          style: TextStyle(color: Colors.black, fontSize: 16),
+                          children: [
+                            TextSpan(
+                              text: ' Login',
+                              style: TextStyle(
+                                color: Colors.green,
+                                fontSize: 16,
+                              ),
+                              recognizer: TapGestureRecognizer()
+                                ..onTap = () {
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (ctx) => Login(),
+                                    ),
+                                  );
+                                },
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/screens/register/register_user.dart
@@ -0,0 +1,38 @@
+import 'dart:convert';
+import 'package:ecobin/shared/modal.dart';
+import 'package:http/http.dart' as http;
+
+Future<Map<String, dynamic>> registerUser({
+  required String name,
+  required String email,
+  required String phone,
+  required String password,
+}) async {
+  final url = Uri.parse('http://192.168.1.162/ecobin/api/register.php');
+
+  final headers = {'Content-Type': 'application/json'};
+
+  final body = json.encode({
+    'name': name,
+    'email': email,
+    'phone': phone,
+    'password': password,
+  });
+
+  final response = await http.post(url, headers: headers, body: body);
+
+  final jsonResponse = json.decode(response.body);
+
+  if (response.statusCode == 201) {
+    return {
+      'success': true,
+      'message': jsonResponse['message'],
+      'user': jsonResponse['user'],
+    };
+  } else {
+    return {
+      'success': false,
+      'message': jsonResponse['message'] ?? 'Something went wrong',
+    };
+  }
+}
