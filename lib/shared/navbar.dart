d869e19899c7e478fb566219ac383f20f98aeb12
Bewizi <toluwalase.bewaji@yahoo.com>
2025-06-25 07:27:06 +0100

    feat: Add bottom navigation bar
    This commit introduces a `Navbar` widget, which implements a bottom navigation bar for the application.
    
    - The `Navbar` is a `StatefulWidget` that takes a `username` string as a required parameter.
    - It manages the `currentIndex` to switch between different screens.
    - The `screens` list currently includes `HomeContent` (passing the `username`) and `Activity`.
    - The `BottomNavigationBar` has four items:
        - Home (Icon: `Icons.home`)
        - Activity (Icon: `Icons.moving`)
        - Requests (Icon: `Icons.delete`)
        - Profile (Icon: `Icons.person`)
    - The selected item and icon color is green, while unselected items are black.
    - The `onTabTapped` function updates the `currentIndex` to display the corresponding screen.

---
+++ lib/shared/navbar.dart
@@ -0,0 +1,57 @@
+import 'package:ecobin/screens/activity/activity.dart';
+import 'package:ecobin/screens/home/home_content.dart';
+import 'package:flutter/material.dart';
+
+class Navbar extends StatefulWidget {
+  const Navbar({super.key, required this.username});
+
+  final String username;
+
+  @override
+  State<Navbar> createState() => _NavbarState();
+}
+
+class _NavbarState extends State<Navbar> {
+  int currentIndex = 0;
+
+  late List<Widget> screens;
+
+  @override
+  void initState() {
+    super.initState();
+    screens = [HomeContent(username: widget.username), Activity()];
+  }
+
+  void onTabTapped(int index) {
+    setState(() {
+      currentIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: screens[currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: currentIndex,
+        onTap: onTabTapped,
+        backgroundColor: Colors.white,
+        selectedLabelStyle: TextStyle(
+          color: Colors.black,
+          fontWeight: FontWeight.w600,
+          fontSize: 20,
+        ),
+        unselectedItemColor: Colors.black,
+        selectedItemColor: Colors.green,
+        showUnselectedLabels: true,
+        selectedIconTheme: IconThemeData(color: Colors.green),
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.moving), label: 'Activity'),
+          BottomNavigationBarItem(icon: Icon(Icons.delete), label: 'Requests'),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
+        ],
+      ),
+    );
+  }
+}
