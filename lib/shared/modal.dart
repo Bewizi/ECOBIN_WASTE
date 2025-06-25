920440bbdf832258267caac9d3d7af6897415c37
Bewizi <toluwalase.bewaji@yahoo.com>
2025-06-25 07:26:39 +0100

    feat: Add ShowModal class for displaying a bottom sheet
    This commit introduces a new class `ShowModal` with a static method `showModal` that displays a modal bottom sheet.
    
    The modal bottom sheet:
    - Has a fixed height of 500 and a white background.
    - Displays a success message:
        - A green circular check icon.
        - "Success! Your're \n Almost There 🎉" text in bold with font size 32.
        - "Thanks for signing up. Let's set up your account so\n we can tailor your EcoBin experience to you!" text with font size 16.
    - Provides two action buttons:
        - `SkipButton` ("Skip for now"): Navigates to the `Home` screen, passing an optional `name`. It removes all previous routes.
        - `CompleteButton` ("Complete Registration"): Navigates to the `UploadPhoto` screen. It removes all previous routes.

---
+++ lib/shared/modal.dart
@@ -0,0 +1,85 @@
+import 'package:ecobin/screens/complete_setup/upload_photo.dart';
+import 'package:ecobin/screens/home/home.dart';
+import 'package:ecobin/screens/home/home_content.dart';
+import 'package:ecobin/shared/button.dart';
+import 'package:flutter/material.dart';
+
+class ShowModal {
+  // static get result => null;
+
+  // final String result;
+
+  // modal
+  static void showModal(BuildContext context, {String? name}) {
+    showModalBottomSheet<void>(
+      context: context,
+      builder: (BuildContext context) {
+        return Container(
+          height: 500,
+          color: Colors.white,
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Container(
+                  width: 32,
+                  height: 32,
+                  decoration: BoxDecoration(
+                    color: Colors.green,
+                    shape: BoxShape.circle,
+                  ),
+                  child: Icon(Icons.check, color: Colors.white),
+                ),
+
+                SizedBox(height: 20),
+
+                Text(
+                  'Success! Your\'re \n Almost There 🎉',
+                  style: TextStyle(
+                    fontSize: 32,
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+
+                SizedBox(height: 20),
+
+                Text(
+                  'Thanks for signing up. Let\'s set up your account so\n'
+                  ' we can tailor your EcoBin experience to you!',
+                  style: TextStyle(fontSize: 16, color: Colors.black),
+                ),
+
+                SizedBox(height: 20),
+
+                SkipButton(
+                  'Skip for now',
+                  onPressed: () {
+                    Navigator.pushAndRemoveUntil(
+                      context,
+                      MaterialPageRoute(builder: (ctx) => Home(name ?? '')),
+                      (route) => false,
+                    );
+                  },
+                ),
+
+                SizedBox(height: 20),
+
+                CompleteButton(
+                  'Complete Registration',
+                  onPressed: () {
+                    Navigator.pushAndRemoveUntil(
+                      context,
+                      MaterialPageRoute(builder: (ctx) => UploadPhoto('')),
+                      (route) => false,
+                    );
+                  },
+                ),
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
