fdaeea5e24a8edae16d842410bea71314e920af3
Toluwalase Bewaji <toluwalase.bewaji@yahoo.com>
2025-07-14 12:53:59 +0100

    feat: Enhance ChoosenWasteType screen UI
    This commit improves the `ChoosenWasteType` screen by:
    
    - Wrapping the content in a `SafeArea` and `SingleChildScrollView` to prevent overflow and ensure content visibility on different screen sizes.
    - Adding a `SizedBox` for better spacing between the header and the radio button list.
    - Increasing the font size of the `ListTile` titles to 20 and setting the text color to black for improved readability.

--- lib/screens/waste_type/choosen_waste_type.dart
+++ lib/screens/waste_type/choosen_waste_type.dart
@@ -18,109 +18,134 @@ class _ChoosenWasteTypeState extends State<ChoosenWasteType> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 20, vertical: 100),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            HeaderText(
-              headingText: 'Household Waste Details',
-              color: Color.fromRGBO(28, 32, 42, 1),
-            ),
-            SubText(
-              text:
-                  'Help us understand what you\'re disposing of\nso we can come prepared with the right tools\nand team',
-              color: Color.fromRGBO(87, 94, 108, 1),
-            ),
-
-            // radio button
-            Column(
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 40),
+            child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                ListTile(
-                  title: Text(' Food wrappers'),
-                  leading: Radio<SingingCharacter>(
-                    value: SingingCharacter.food,
-                    groupValue: _character,
-                    onChanged: (SingingCharacter? value) {
-                      setState(() {
-                        _character = value;
-                      });
-                    },
-                  ),
+                HeaderText(
+                  headingText: 'Household Waste Details',
+                  color: Color.fromRGBO(28, 32, 42, 1),
+                ),
+                SubText(
+                  text:
+                      'Help us understand what you\'re disposing of\nso we can come prepared with the right tools\nand team',
+                  color: Color.fromRGBO(87, 94, 108, 1),
                 ),
 
-                ListTile(
-                  title: Text('Used tissues or napkins'),
-                  leading: Radio<SingingCharacter>(
-                    value: SingingCharacter.recyclables,
-                    groupValue: _character,
-                    onChanged: (SingingCharacter? value) {
-                      setState(() {
-                        _character = value;
-                      });
-                    },
-                  ),
+                SizedBox(height: 16),
+                // radio button
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    ListTile(
+                      title: Text(' Food wrappers'),
+                      titleTextStyle: TextStyle(
+                        fontSize: 20,
+                        color: Colors.black,
+                      ),
+                      leading: Radio<SingingCharacter>(
+                        value: SingingCharacter.food,
+                        groupValue: _character,
+                        onChanged: (SingingCharacter? value) {
+                          setState(() {
+                            _character = value;
+                          });
+                        },
+                      ),
+                    ),
+
+                    ListTile(
+                      title: Text('Used tissues or napkins'),
+                      titleTextStyle: TextStyle(
+                        fontSize: 20,
+                        color: Colors.black,
+                      ),
+                      leading: Radio<SingingCharacter>(
+                        value: SingingCharacter.recyclables,
+                        groupValue: _character,
+                        onChanged: (SingingCharacter? value) {
+                          setState(() {
+                            _character = value;
+                          });
+                        },
+                      ),
+                    ),
+
+                    ListTile(
+                      title: Text('Damaged household items'),
+                      titleTextStyle: TextStyle(
+                        fontSize: 20,
+                        color: Colors.black,
+                      ),
+                      leading: Radio<SingingCharacter>(
+                        value: SingingCharacter.organic,
+                        groupValue: _character,
+                        onChanged: (SingingCharacter? value) {
+                          setState(() {
+                            _character = value;
+                          });
+                        },
+                      ),
+                    ),
+
+                    ListTile(
+                      title: Text('Old clothes or fabrics'),
+                      titleTextStyle: TextStyle(
+                        fontSize: 20,
+                        color: Colors.black,
+                      ),
+                      leading: Radio<SingingCharacter>(
+                        value: SingingCharacter.electronic,
+                        groupValue: _character,
+                        onChanged: (SingingCharacter? value) {
+                          setState(() {
+                            _character = value;
+                          });
+                        },
+                      ),
+                    ),
+
+                    ListTile(
+                      title: Text('All kind of household waste'),
+                      titleTextStyle: TextStyle(
+                        fontSize: 20,
+                        color: Colors.black,
+                      ),
+
+                      leading: Radio<SingingCharacter>(
+                        value: SingingCharacter.medical,
+                        groupValue: _character,
+                        onChanged: (SingingCharacter? value) {
+                          setState(() {
+                            _character = value;
+                          });
+                        },
+                      ),
+                    ),
+                  ],
                 ),
 
-                ListTile(
-                  title: Text('Damaged household items'),
-                  leading: Radio<SingingCharacter>(
-                    value: SingingCharacter.organic,
-                    groupValue: _character,
-                    onChanged: (SingingCharacter? value) {
-                      setState(() {
-                        _character = value;
-                      });
-                    },
-                  ),
-                ),
+                SizedBox(height: 120),
 
-                ListTile(
-                  title: Text('Old clothes or fabrics'),
-                  leading: Radio<SingingCharacter>(
-                    value: SingingCharacter.electronic,
-                    groupValue: _character,
-                    onChanged: (SingingCharacter? value) {
-                      setState(() {
-                        _character = value;
-                      });
-                    },
-                  ),
-                ),
-
-                ListTile(
-                  title: Text('All kind of household waste'),
-
-                  leading: Radio<SingingCharacter>(
-                    value: SingingCharacter.medical,
-                    groupValue: _character,
-                    onChanged: (SingingCharacter? value) {
-                      setState(() {
-                        _character = value;
-                      });
-                    },
-                  ),
+                CompleteButton(
+                  'Next',
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) {
+                          return PickupDetailsDetails();
+                        },
+                      ),
+                    );
+                  },
                 ),
               ],
             ),
-
-            SizedBox(height: 120),
-
-            CompleteButton(
-              'Next',
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) {
-                      return PickupDetailsDetails();
-                    },
-                  ),
-                );
-              },
-            ),
-          ],
+          ),
         ),
       ),
     );
